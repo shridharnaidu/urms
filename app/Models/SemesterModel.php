eb5ea51b852c10b0b637472ce4709fd5416c665e
shridhar-demo <shridharnaidu2001@gmail.com>
2025-06-28 00:30:03 +0530

    Added Semiste module

--- app/Models/SemesterModel.php
+++ app/Models/SemesterModel.php
@@ -1,1 +1,11 @@
 <?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class SemesterModel extends Model
+{
+    protected $table = 'semesters';
+    protected $allowedFields = ['name', 'course_id'];
+}
